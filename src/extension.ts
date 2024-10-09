4b5d312518f86dec7de1b0c5709ab540ff2a2780
Martins Folarin <folarin@engineer.com>
2024-10-08 21:07:49 -0400

    A: added initial implementation of feature set

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,53 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+// src/extension.ts
+import { commands, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument, window, workspace } from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
+	const statusBar = window.createStatusBarItem(StatusBarAlignment.Right, 100);
+	statusBar.show();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-file-stats" is now active!');
+	const updateStatusBar = async () => {
+		const editor = window.activeTextEditor;
+		if (!editor) return;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-file-stats.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-file-stats!');
-	});
+		const doc = editor.document;
+		const selections = editor.selections;
 
-	context.subscriptions.push(disposable);
+		const lines = doc.lineCount;
+		const words = doc.getText().split(/\s+/).length;
+		const bytes = doc.getText().length;
+		const selectedChars = selections.reduce((total, selection) => total + doc.getText(selection).length, 0);
+		const fileSize = await getFileSize(doc.fileName);
+		const formattedFileSize = formatFileSize(fileSize);
+
+		const statusBarText = `File Lines: ${lines} Words: ${words} Sel: ${selectedChars} (${selections.length} selections)`;
+		statusBar.text = statusBarText;
+		statusBar.tooltip = `File Size: ${formattedFileSize}\nLines: ${lines}\nWords: ${words}\nSelected Characters: ${selectedChars}`;
+	};
+
+	const getFileSize = async (fileName: string) => {
+		if (!fileName) return 0;
+		const file = await workspace.openTextDocument(fileName);
+		return file.getText().length;
+	};
+
+	const formatFileSize = (bytes: number) => {
+		if (bytes < 1024) return `${bytes} B`;
+		if (bytes < 1024 ** 2) return `${(bytes / 1024).toFixed(2)} KB`;
+		if (bytes < 1024 ** 3) return `${(bytes / 1024 ** 2).toFixed(2)} MB`;
+		return `${(bytes / 1024 ** 3).toFixed(2)} GB`;
+	};
+
+	window.onDidChangeActiveTextEditor(updateStatusBar);
+	window.onDidChangeTextEditorSelection(updateStatusBar);
+	workspace.onDidChangeTextDocument(updateStatusBar);
+
+	context.subscriptions.push(statusBar);
+
+	context.subscriptions.push(
+		commands.registerCommand('file-stats.toggle', () => {
+			statusBar.hide();
+		})
+	);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
